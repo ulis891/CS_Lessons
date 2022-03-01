2a092dc012244e8dcdd084c1447bde07012ddcb9
Ulis89 <ulis89@yandex.ru>
2022-03-01 23:56:32 +0300

    upd S2 semProject

--- S2/semProject_1/Program.cs
+++ S2/semProject_1/Program.cs
@@ -44,20 +44,70 @@
 
 // Является ли первое число квадратом второго (через функцию)
 
-bool isSquare(int number1, int number2){
-    return number1 == number2 * number2; 
+// bool isSquare(int number1, int number2){
+//     return number1 == number2 * number2; 
+// }
+
+// Console.WriteLine("Введите число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (isSquare(number1, number2))
+// {
+//     Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+// }
+
+
+// //Задача 14. Найти третью цифру числа или сообщить, что её нет
+
+// int ThirdDigit (int number)
+// {
+//     int digit3 = number % 10;
+//     return digit3;
+// }
+
+// Console.WriteLine("Введите трёхзначное число: ");
+// int newNumber = Convert.ToInt32(Console.ReadLine());
+
+// if (newNumber > 99 & newNumber < 1000)
+// {
+//     Console.WriteLine($"Третья цифра числа {newNumber} я вляется {ThirdDigit(newNumber)}");
+// }
+
+// else
+// {
+//     Console.WriteLine("В этом числе нет третьей цифры");
+// }
+
+
+
+//Задача 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+void weekend (int number)
+{
+    if (number > 5)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Работать!!!");
+    }
 }
 
-Console.WriteLine("Введите число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер дня недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
 
-if (isSquare(number1, number2))
+if (dayNumber <= 7 & dayNumber >= 1)
 {
-    Console.WriteLine($"Число {number1} является квадратом числа {number2}");
+    weekend(dayNumber);
 }
 else
 {
-    Console.WriteLine($"Число {number1} не является квадратом числа {number2}");
+    Console.WriteLine("Вы ввели неверное число!");
 }
