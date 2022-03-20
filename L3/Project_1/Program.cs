373f03836bddc948d1180b16226bd2500723dd29
Aleksandr Kostromin <ulis89@yandex.ru>
2022-03-20 21:09:46 +0300

    add L3

---
+++ L3/Project_1/Program.cs
@@ -0,0 +1,34 @@
+﻿//сортировка от большего к мньшему в заданном массиве
+
+int [] arr = {1,1,23,53,4,23,1,7,65,3,47,34,77,3};
+
+void PrintArray (int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Sort (int [] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(array: arr);
+Sort(arr);
+PrintArray(arr);
