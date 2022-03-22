64f20d7e77d8d9e29cf272af5d04cb90840cfe63
Aleksandr Kostromin <ulis89@yandex.ru>
2022-03-23 01:48:38 +0300

    upd DZ_4.1

--- S4/DZ_4.2/DZ_4.2.cs
+++ S4/DZ_4.2/DZ_4.2.cs
@@ -1,18 +1,12 @@
 ﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-// Console.WriteLine();
-// Console.WriteLine("Введите число: ");
-// int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
-// string strNum = Convert.ToString(num);
-// int result = 0;
-// for (int i = 0; i < strNum.Length; i++)
-// {
-//     result += Convert.ToInt32(strNum[i]);
-//     Console.WriteLine(Convert.ToInt32(strNum[i]));
-// }
-// Console.WriteLine(result);
-
-string strNum = "123";
-Console.WriteLine(strNum[0]);       // выводит первый элемент строки
-Console.WriteLine(strNum[0].GetTypeCode());
-Console.WriteLine(Convert.ToInt32(strNum[0]));      // после конвертации выводит 49!!! почему?!
+Console.WriteLine();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string strNum = Convert.ToString(Math.Abs(num));
+int result = 0;
+for (int i = 0; i < strNum.Length; i++)
+{
+    result += Convert.ToInt32(strNum[i].ToString());        //Долго мучился с этим моментом так как при конвертации из символа в число выдовался AscII код символа. Сделал через ToString.
+}
+Console.WriteLine($"Сумма цифр числа {num} равна {result}");
