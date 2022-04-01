c42676101e99fdb0081bead0096449bac467a16e
Aleksandr Kostromin <ulis89@yandex.ru>
2022-04-01 19:22:07 +0300

    add DZ_6.1 and DZ_6.2

---
+++ S6/DZ_6.1/DZ_6.1.cs
@@ -0,0 +1,56 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры или задает самостоятельно M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine();
+Console.WriteLine("Сколько чисел будет в проверенно?");
+int Len = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[] arr = new int [Len];
+
+
+void newArray(int Length)
+{
+    for (int i = 0; i < Length; i++)
+    {
+        Console.WriteLine($"Введите число №{i+1}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine();
+    }
+}
+
+int PositiveNum (int[] array)
+{   
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+void PrintAnswer(int num)
+{
+    if (num == 0)
+    {
+        Console.WriteLine($"В массиве нет положительных чисел");
+        return;
+    }
+    if (num % 10 == 1 & num != 11)
+    {
+        Console.WriteLine($"В массиве {num} положительное число");
+        return;
+    }
+    if (num % 10 >= 2 & num % 10 <= 4 & (num % 100 != 11 & num % 100 != 12 & num % 100 != 13 & num % 100 != 14 ))
+    {
+        Console.WriteLine($"В массиве {num} положительных числа");
+        }
+    else
+    {
+        Console.WriteLine($"В массиве {num} положитлеьных чисел");
+    }
+}
+
+newArray(Len);
+PrintAnswer(PositiveNum(arr));
---
+++ S6/DZ_6.2/DZ_6.2.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем или задаются вручную.*/
+
+Console.ResetColor();
+Console.WriteLine();
+Console.WriteLine("Для уравнений  y=k1*x+b1  и  y=k2*x+b2  введите следующие значения: ");
+Console.WriteLine();
+Console.WriteLine("Введите значенине k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значенине b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значенине k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значенине b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine();
+double x;
+double y;
+if (k1 == k2)
+{
+    if (b1 == b2)
+    {
+        Console.WriteLine("Прямые одинаковые");
+    }
+    else
+    {
+        Console.WriteLine("Прямые параллельные");
+    }
+}
+else
+{
+    if (b1 == b2)
+    {
+        Console.WriteLine($"Точкой пересечения двух прямых является координата ({0} .  {b1})");
+    }
+    else
+    {
+        x = (b2-b1)/(k1-k2);
+        y = k2 * x + b2;
+        Console.WriteLine($"Точкой пересечения двух прямых является координата ({x:f2} .  {y:f2})");        
+    }
+}
+Console.WriteLine();
