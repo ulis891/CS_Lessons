324788ec8ffa6018ab0df8cdf5e03a17700785fd
Aleksandr Kostromin <ulis89@yandex.ru>
2022-04-11 01:06:07 +0300

    add S9, DZ_9.1

---
+++ S9/DZ_9.1/DZ_9.1.cs
@@ -0,0 +1,179 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4(массив заполняется по часовой стрелке от периферии к центру).
+
+
+Console.WriteLine();
+Console.WriteLine("Сколько строк будет в массиве?");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine("Сколько столбцов будет в массиве?");
+int colums = Convert.ToInt32(Console.ReadLine());
+int[,] table = new int[rows, colums];
+int num = 1;
+int iIndex = 0;
+int jIndex = 0;
+int saveColumsIndex;
+int saveRowIndex;
+
+
+void Spiral(int[,] matr)
+{
+    if (num < matr.GetLength(0) * matr.GetLength(1))
+    {
+        int saveColumsIndex = 0;
+        int j = jIndex;
+        for (int i = j; i < matr.GetLength(1); i++)
+        {
+            matr[j, i] = num;
+            num++;
+            jIndex = i;
+        }
+        for (int i = matr.GetLength(0) - jIndex; i < matr.GetLength(0); i++)
+        {
+            matr[i, jIndex] = num;
+            num++;
+            saveColumsIndex = i;
+        }
+        jIndex = saveColumsIndex;
+        for (int i = jIndex-1; i >= matr.GetLength(1) - jIndex-1; i--)
+        {
+            matr[jIndex, i] = num;
+            num++;
+            saveColumsIndex = i;
+        }
+        jIndex = saveColumsIndex;
+        for (int i = jIndex; i > matr.GetLength(1); i--)
+        {
+            matr[i, jIndex] = num;
+            num++;
+            saveColumsIndex = i;
+        }
+        jIndex = saveColumsIndex;
+    }
+}
+
+
+
+
+
+// void FillArray(int[,] matr)
+// {
+//     int num = 1;
+//     int saveRowIndex = 0;
+//     int saveColumsIndex = 0;
+
+//     for (int i; i < matr.GetLength(1); i++)
+//     {
+//         matr[i, j] = num;
+//         num++;
+//         saveRowIndex = i;
+//         saveColumsIndex = j;
+//     }
+
+// }
+
+// void MoveRight(int[,] matr, int iPosition, int jPosition)
+// {
+//     for (int i = iPosition; i < matr.GetLength(1); i++)
+//     {
+//         if (num < matr.GetLength(0) * matr.GetLength(1))
+//         {
+//             int j = jPosition;
+//             matr[j, i] = num;
+//             num++;
+//             saveRowIndex = i;
+//             saveColumsIndex = j;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//         MoveDown(matr, saveRowIndex, saveColumsIndex);
+//     }
+
+
+
+// }
+
+// void MoveDown(int[,] matr, int iPosition, int JPosition)
+// {
+//     for (int i = iPosition; i < matr.GetLength(1); i++)
+//     {
+//         if (num < matr.GetLength(0) * matr.GetLength(1))
+//         {
+//             int j = jPosition;
+//             matr[i, j] = num;
+//             num++;
+//             saveRowIndex = i;
+//             saveColumsIndex = j;
+//         }
+//         else
+//         {
+//             break;
+//         }
+//         // MoveDown(matr, saveRowIndex, saveColumsIndex);
+//     }
+// }
+
+// // void MoveLeft(int[,] matr, int iPosition, int JPosition)
+// // {
+// //     for (int i; i < matr.GetLength(1); i++)
+// //     {
+// //         matr[i, j] = num;
+// //         num++;
+// //         saveRowIndex = i;
+// //         saveColumsIndex = j;
+// //     }
+// // }
+
+// // // void MoveUp(int[,] matr, int iPosition, int JPosition)
+// // {
+// //     for (int i; i < matr.GetLength(1); i++)
+// //     {
+// //         matr[i, j] = num;
+// //         num++;
+// //         saveRowIndex = i;
+// //         saveColumsIndex = j;
+// //     }
+// // }
+
+
+// for (int i = 0; i < length; i++)
+// {
+//     for (int j = 0; j < length; j++)
+//     {
+//         if (matr[i, j] == 0 & j < matr.GetLength(1))
+//         {
+//             matr[i, j] = num;
+//             num++;
+//             jIndex = j;
+//         }
+//     }
+//     for (int j = jIndex; j < length; j++)
+//     {
+
+//     }
+// }
+
+
+
+
+void PrintArray(int[,] matr)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] > 9) { Console.Write($"{matr[i, j]}  "); }
+            else { Console.Write($"{matr[i, j]}   "); }
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Spiral(table);
+Console.WriteLine();
+Console.WriteLine("Изначальный массив:");
+PrintArray(table);
---
+++ S9/Project/Program.cs
@@ -0,0 +1,146 @@
+﻿/* Задача 59. Задайте двумерный массив из целых чисел. 
+Напишите программу, которая удалит строку и столбец, на пересичении которыйх расположен наименьший элемент*/
+
+// Console.WriteLine();
+// Console.WriteLine("Сколько строк будет в массиве?");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+// Console.WriteLine("Сколько столбцов будет в массиве?");
+// int colums = Convert.ToInt32(Console.ReadLine());
+// int[,] table = new int[rows, colums];
+// int iIndex = 0;
+// int jIndex = 0;
+
+// void FillArray(int[,] matr)
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             matr[i, j] = new Random().Next(1, 100);
+//         }
+//     }
+// }
+
+// void PrintArray(int[,] matr)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             if (matr[i, j] > 9) { Console.Write($"{matr[i, j]}  "); }
+//             else { Console.Write($"{matr[i, j]}   "); }
+
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void FindMinElement(int[,] matr)
+// {
+//     int minNum = matr[0, 0];
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)
+//         {
+//             if (matr[i, j] < minNum)
+//             {
+//                 minNum = matr[i, j];
+//                 iIndex = i;
+//                 jIndex = j;
+//             }
+//         }
+//     }
+// }
+
+// void PrintNewArray(int[,] matr, int minRowIndex, int minColumIndex)
+// {
+//     Console.WriteLine();
+//     for (int i = 0; i < matr.GetLength(0); i++)
+//     {
+//         if (i != minRowIndex)
+//         {
+//             for (int j = 0; j < matr.GetLength(1); j++)
+//             {
+//                 if (j != minColumIndex)
+//                 {
+//                     if (matr[i, j] > 9) Console.Write($"{matr[i, j]}  ");
+//                     else Console.Write($"{matr[i, j]}   ");
+//                 }
+//             }
+//             Console.WriteLine();
+//         }        
+//     }
+//     Console.WriteLine();
+// }
+
+// FillArray(table);
+// Console.WriteLine();
+// Console.WriteLine("Изначальный массив:");
+// PrintArray(table);
+// FindMinElement(table);
+// Console.WriteLine("Массив с вырезаной строкой и столбцом минимильного элемента:");
+// PrintNewArray(table, iIndex, jIndex);
+
+
+/* Задача 61. Вывесит первые N строк треугольника Паскаля. Сделать вывод равнобедренного треугольника*/
+
+Console.WriteLine();
+Console.WriteLine("Сколько строк треугольника Паскаля вывести?");
+int rows = Convert.ToInt32(Console.ReadLine());
+int[,] table = new int[rows, rows + 1];
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        matr[i, 0] = 1;
+        for (int j = 1; j < matr.GetLength(1); j++)
+        {
+            if (i > 0)
+            {
+                matr[i, j] = matr[i - 1, j] + matr[i - 1, j - 1];
+            }
+        }
+    }
+    // for (int i = 1; i < matr.GetLength(0); i++)
+    // {
+    //     for (int j = 1; j < matr.GetLength(1); j++)
+    //     {
+    //         matr[i, j] = matr[i - 1, j] + matr[i - 1, j - 1];
+    //     }
+    // }
+}
+void PrintArray(int[,] matr)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (matr[i, j] > 9) { Console.Write($"{matr[i, j]}  "); }
+            else { Console.Write($"{matr[i, j]}   "); }
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void PrintArray2(int[,] matr)
+{
+    Console.WriteLine();
+    for (int i = matr.GetLength(0); i > 0; i--)
+    {
+        for (int j = matr.GetLength(1); j > 0; j--)
+        {
+
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+FillArray(table);
+PrintArray(table);
