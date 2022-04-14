48bb874e19a77af37677a05a115e3f39a3b408ec
Aleksandr Kostromin <ulis89@yandex.ru>
2022-04-14 18:52:39 +0300

    Add L6

---
+++ L6/Project_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Cache;
+
+class Program
+{
+    static public void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ L6/Project_2VS/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Net.Cache;
+using ListMaster;
+
+namespace ConsoleVS
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            ArrayMaster am = new ArrayMaster();
+            for (int i = 0; i < 10; i++)
+            {
+                am.Add(new Random().Next(1,5));
+            }
+            
+            for (int i = 0; i < am.GetCount(); i++)
+            {
+                Console.WriteLine(am.GetValue(i));
+            }
+        }
+
+    }   
+}
