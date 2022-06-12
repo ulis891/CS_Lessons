d2f914283a3bd609ad87c2eb75e911be59d8ae66
Aleksandr Kostromin <ulis89@yandex.ru>
2022-06-12 23:51:26 +0300

    RTY

--- S1/SemProject/Program.cs
+++ S1/SemProject/Program.cs
@@ -1,6 +1,16 @@
-﻿// Задача 1. Вывести квадрат числа
-Console.WriteLine("Введите число: ");
-// Конвертируем строку в число
-int numberValue = Convert.ToInt32(Console.ReadLine());
-int result = numberValue*numberValue;
-Console.WriteLine(result);
+﻿// // Задача 1. Вывести квадрат числа
+// Console.WriteLine("Введите число: ");
+// // Конвертируем строку в число
+// int numberValue = Convert.ToInt32(Console.ReadLine());
+// int result = numberValue*numberValue;
+// Console.WriteLine(result);
+
+
+int a = 6;
+int b = 25;
+if (b == a*a)
+{
+    Console.WriteLine("fafdf");
+}
+
+else Console.WriteLine(".......");
